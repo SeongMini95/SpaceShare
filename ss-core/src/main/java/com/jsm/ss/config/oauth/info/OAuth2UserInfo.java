ad82929d45cdfb73dc41d6257f76b84ecce92259
SeongMini95 <modify879@gmail.com>
2022-07-11 23:55:25 +0900

    feat: add OAuth2UserInfo

---
+++ ss-core/src/main/java/com/jsm/ss/config/oauth/info/OAuth2UserInfo.java
@@ -0,0 +1,19 @@
+package com.jsm.ss.config.oauth.info;
+
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+public abstract class OAuth2UserInfo {
+
+    protected Map<String, Object> attributes;
+
+    public OAuth2UserInfo(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    public abstract String getId();
+
+    public abstract String getEmail();
+}
---
+++ ss-core/src/main/java/com/jsm/ss/config/oauth/info/impl/KakaoOAuth2UserInfo.java
@@ -0,0 +1,28 @@
+package com.jsm.ss.config.oauth.info.impl;
+
+import com.jsm.ss.config.oauth.info.OAuth2UserInfo;
+
+import java.util.Map;
+
+public class KakaoOAuth2UserInfo extends OAuth2UserInfo {
+
+    public KakaoOAuth2UserInfo(Map<String, Object> attributes) {
+        super(attributes);
+    }
+
+    @Override
+    public String getId() {
+        return (String) attributes.get("id");
+    }
+
+    @Override
+    public String getEmail() {
+        Map<String, Object> properties = (Map<String, Object>) attributes.get("properties");
+
+        if (properties == null) {
+            return null;
+        }
+
+        return (String) properties.get("account_email");
+    }
+}
